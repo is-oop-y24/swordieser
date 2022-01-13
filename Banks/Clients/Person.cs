e26ce34d2b7684356776870dbc67636ba8c66142
Sergey Ivanov <swordie.ser@gmail.com>
2022-01-13 21:40:43 +0300

    feat: changed client system

--- Banks/Clients/Person.cs
+++ Banks/Clients/Person.cs
@@ -1,39 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Text;
 using Banks.Accounts;
 
 namespace Banks.Clients
 {
     public class Person
     {
-        private List<Account> _accounts;
+        private List<IAccount> _accounts;
 
-        public Person(string name, string surname)
+        public Person(string name, string surname, string address, long passport)
         {
-            _accounts = new List<Account>();
+            _accounts = new List<IAccount>();
             Name = name;
             Surname = surname;
+            Address = address;
+            Passport = passport;
+            CheckDoubtfulness();
         }
 
         public string Name { get; }
 
         public string Surname { get; }
 
-        public string Address { get; set; } = string.Empty;
+        private string Address { get; set; }
 
-        public long Passport { get; set; } = 0;
+        private long Passport { get; set; }
 
-        public bool Doubtful { get; set; } = true;
+        private bool Doubtful { get;  set; } = true;
 
-        public void AddNewAccount(Account account)
+        public void AddNewAccount(IAccount account)
         {
             _accounts.Add(account);
         }
 
-        public ReadOnlyCollection<Account> GetAccounts()
+        public ReadOnlyCollection<IAccount> GetAccounts()
         {
             return _accounts.AsReadOnly();
+        }
+
+        public void SetAddress(string address)
+        {
+            Address = address;
+            CheckDoubtfulness();
+        }
+
+        public void SetPassport(long passport)
+        {
+            Passport = passport;
+            CheckDoubtfulness();
         }
 
         public void CheckDoubtfulness()
@@ -41,10 +57,11 @@ namespace Banks.Clients
             if (Address != string.Empty && Passport != 0)
             {
                 Doubtful = false;
-                foreach (Account account in _accounts)
+                foreach (IAccount account in _accounts)
                 {
-                    account.MaxWithdraw = 0;
-                    account.MaxTransfer = 0;
+                    account.SetMaxWithdraw(0);
+                    account.SetMaxTransfer(0);
+                    Doubtful = false;
                 }
             }
         }
