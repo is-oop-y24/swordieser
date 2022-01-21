31e2e627adecda87e69afe485fbff7611fe30a68
Sergey Ivanov <swordie.ser@gmail.com>
2022-01-21 21:34:58 +0300

    feat: added loggers

---
+++ BackupsExtra/Interfaces/IBackupLogger.cs
@@ -0,0 +1,9 @@
+﻿using Serilog.Core;
+
+namespace BackupsExtra.Interfaces
+{
+    public interface IBackupLogger
+    {
+        Logger CreateLog(bool withTimeStamp, string message);
+    }
+}
---
+++ BackupsExtra/Loggers/FileLogger.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using BackupsExtra.Interfaces;
+using Serilog;
+using Serilog.Core;
+
+namespace BackupsExtra.Loggers
+{
+    public class FileLogger : IBackupLogger
+    {
+        public Logger CreateLog(bool withTimeStamp, string message)
+        {
+            string template = withTimeStamp
+                ? "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message}{NewLine}{Exception}"
+                : "[{Level:u3}] {Message}{NewLine}{Exception}";
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "log.txt");
+            Logger logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.File(path, outputTemplate: template)
+                .CreateLogger();
+            logger.Information(message);
+            return logger;
+        }
+    }
+}
